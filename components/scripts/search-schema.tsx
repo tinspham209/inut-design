ec5147df79a2ce71fdde78c88e0148c3ea0a9696
tin_pham <tindeptrai1@gmail.com>
2023-04-28 17:05:04 +0700

    fix(seo): search schema

--- components/scripts/search-schema.tsx
+++ components/scripts/search-schema.tsx
@@ -2,7 +2,7 @@ export const jsonSearchSchema = `
 {
   "@context": "http://schema.org/",
   "@type": "Product",
-  "name": "INUT Design - Tiệm may đo skin laptop theo yêu cầu, Cửa Hàng Thời Trang Dành Cho Laptop, INUT Design, Inut skin, inutdesign, inutskin, skin laptop, skin laptop da nang, skin laptop đà nẵng",
+  "name": "INUT Design - Tiệm may đo skin laptop theo yêu cầu, INUT Design, Inut skin, skin laptop, skin laptop da nang",
   "description": "INUT Design - Cửa Hàng Thời Trang Dành Cho Laptop Tại Đà Nẵng, skin laptop da nang, skin laptop đà nẵng",
   "sku": "skin-laptop-da-nang",
   "url": "https://inutdesign.com",
@@ -20,9 +20,13 @@ export const jsonSearchSchema = `
   },
   "offers": {
     "@type": "Offer",
+    "offerCount": 5,
+    "lowPrice": 60000,
+    "highPrice": 150000,
     "price": "100000",
     "priceCurrency": "VND",
     "priceValidUntil": "2024-12-31",
+    "itemCondition": "https://schema.org/UsedCondition",
     "availability": "http://schema.org/InStock",
     "seller": {
       "@type": "Organization",
@@ -32,20 +36,55 @@ export const jsonSearchSchema = `
       "@type": "OfferShippingDetails",
       "shippingMethod": "Free shipping",
       "shippingCost": "0",
-      "deliveryTime": "3-5 business days"
+      "shippingRate": {
+        "@type": "MonetaryAmount",
+        "value": "0",
+        "currency": "VND"
+      },
+      "shippingDestination": {
+        "@type": "DefinedRegion",
+        "addressCountry": "VN",
+        "addressRegion": ["DN"]
+      },
+      "deliveryTime": {
+        "@type": "ShippingDeliveryTime",
+        "handlingTime": {
+          "@type": "QuantitativeValue",
+          "minValue": 0,
+          "maxValue": 1,
+          "unitCode": "DAY"
+        },
+        "transitTime": {
+          "@type": "QuantitativeValue",
+          "minValue": 1,
+          "maxValue": 5,
+          "unitCode": "DAY"
+        }
+      }
     },
-    "hasMerchantReturnPolicy": true
+    "hasMerchantReturnPolicy": {
+      "@type": "MerchantReturnPolicy",
+      "refundsAllowed": true,
+      "returnPolicyCategory": "http://schema.org/RefundReturnFees",
+      "returnFees": {
+        "@type": "MonetaryAmount",
+        "currency": "VND",
+        "value": "15000"
+      },
+      "applicableCountry": "VN"
+    }
   },
   "aggregateRating": {
     "@type": "AggregateRating",
-    "ratingValue": "4.5",
-    "reviewCount": "100"
+    "ratingValue": 4.5,
+    "reviewCount": 89
   },
   "review": {
     "@type": "Review",
     "reviewRating": {
       "@type": "Rating",
-      "ratingValue": "4.5"
+      "ratingValue": 4.5,
+      "bestRating": 5
     },
     "author": {
       "@type": "Person",
@@ -54,6 +93,31 @@ export const jsonSearchSchema = `
     "datePublished": "2023-03-31",
     "description": "í, lướt fb thấy shop custom xịn sò từng phím còn đang giảm giá nên vẫn sắp xếp đi luôn :)))) trong lúc ngồi đợi vẫn tìm được mấy góc chill. máy mình thì bánh bèo nhưng siêu ưng nè"
   },
+  "positiveNotes": {
+    "@type": "ItemList",
+    "itemListElement": [
+      {
+        "@type": "ListItem",
+        "position": 1,
+        "name": "Consistent results"
+      },
+      {
+        "@type": "ListItem",
+        "position": 2,
+        "name": "Still sharp after many uses"
+      }
+    ]
+  },
+  "negativeNotes": {
+    "@type": "ItemList",
+    "itemListElement": [
+      {
+        "@type": "ListItem",
+        "position": 1,
+        "name": "Không có skin PPF"
+      },
+    ]
+  }
   "potentialAction": {
     "@type": "SearchAction",
     "target": "https://inutdesign.com/products?q={search_term_string}",
