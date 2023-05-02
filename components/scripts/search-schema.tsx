7b3eb6990b9ad7cccde0a5fa352691c46a6a42dc
tin_pham <tindeptrai1@gmail.com>
2023-05-02 20:51:53 +0700

    fix: search schema

--- components/scripts/search-schema.tsx
+++ components/scripts/search-schema.tsx
@@ -7,13 +7,13 @@ export const jsonSearchSchema = `
 	"sku": "skin-laptop-da-nang",
 	"url": "https://inutdesign.com",
 	"image": [
-		"{img_url_1}",
-		"{img_url_2}",
-		"{img_url_3}",
-		"{img_url_4}",
-		"{img_url_5}",
-		"{img_url_6}"
-	],
+		"https://inutdesign.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fsoud11bs%2Fproduction%2Fd8eb4f865977e824fe06dd517f11744d8df3fbf8-2048x1536.webp%3Fw%3D800&w=3840&q=75",
+    "https://inutdesign.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fsoud11bs%2Fproduction%2F3744bb790c106b9557532be1813e37fbffaef6c1-2560x1920.webp%3Fw%3D800&w=3840&q=75",
+    "https://inutdesign.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fsoud11bs%2Fproduction%2Fc0e739cdf286f87e92534487ae7be67ff82b4565-2560x1920.webp%3Fw%3D800&w=3840&q=75",
+    "https://inutdesign.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fsoud11bs%2Fproduction%2F6fa1827094f475f9542d2a56dca9a4bcef994ad0-2560x1962.webp%3Fw%3D800&w=3840&q=75",
+    "https://inutdesign.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fsoud11bs%2Fproduction%2F9a072bdd73b8d7b4c20cfed109eb3f109f99d866-2560x1924.webp%3Fw%3D800&w=3840&q=75",
+    "https://inutdesign.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fsoud11bs%2Fproduction%2F7c16454bd72f46dc28d4e5dd79720b16a83ae85a-2560x1912.webp%3Fw%3D800&w=3840&q=75"
+  ],
 	"brand": {
 		"@type": "Brand",
 		"name": "INUT Design"
@@ -65,12 +65,13 @@ export const jsonSearchSchema = `
 		"hasMerchantReturnPolicy": {
 			"@type": "MerchantReturnPolicy",
 			"refundsAllowed": true,
-			"returnPolicyCategory": "http://schema.org/RefundReturnFees",
+			"returnPolicyCategory": "http://schema.org/MerchantReturnFiniteReturnWindow",
 			"returnFees": {
 				"@type": "MonetaryAmount",
 				"currency": "VND",
 				"value": 15000
 			},
+			"returnMethod": "https://schema.org/ReturnByMail",
 			"applicableCountry": "VN"
 		}
 	},
