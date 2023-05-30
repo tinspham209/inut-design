ee8df44faa13601311154c04afebf2fe610418d8
tin_pham <tindeptrai1@gmail.com>
2023-05-30 17:21:36 +0700

    build: fix build

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -10,9 +10,6 @@ import { toast } from "react-hot-toast";
 
 const SignIn: NextPageWithLayout = () => {
 	const [isLoading, setIsLoading] = React.useState(false);
-	console.log("NEXTAUTH_URL: ", process.env.NEXTAUTH_URL);
-	console.log("NEXTAUTH_URL: ", process.env.NEXTAUTH_SECRET);
-
 	const router = useRouter();
 	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
